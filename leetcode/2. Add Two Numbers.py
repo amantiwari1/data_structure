ab209688dcdf4787db9c7c69584f4af833c55a42
Aman Tiwari <53963065+amantiwari1@users.noreply.github.com>
2020-12-05 23:33:37 +0530

    Create 2. Add Two Numbers.py

---
+++ leetcode/2. Add Two Numbers.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if  l1.val == 0 and l1.next == None and l2.next == None and l2.val == 0:
+            return ListNode(0)
+        a = l1.val
+        b = l2.val
+        c = 0
+
+        if a+b >= 10:
+            c = 1
+        nodes = ListNode((a+b)%10)
+        stores = nodes
+        l1 = l1.next
+        l2 = l2.next
+        while l1 or l2:
+            a = 0
+            b = 0
+            if l1:
+                a = l1.val
+                l1 = l1.next
+            if l2:
+                b = l2.val
+                l2= l2.next
+          
+            nodes.next = ListNode((a+b+c)%10)
+            if a+b+c >= 10:
+                c = 1
+            else:
+                c = 0
+            nodes = nodes.next
+                
+        if nodes.val == 0 or a+b >= 10:
+            nodes.next = ListNode(1)
+        return stores 
+            
+            
